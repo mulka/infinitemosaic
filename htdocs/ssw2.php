cd955c1cc8ff67b8f35374d00b6bff98c3ececd8
Kyle Mulka <repalviglator@yahoo.com>
2009-02-13 03:00:46 +0000

    /ssw2 opens Seadragon on iPhone now

--- htdocs/ssw2.php
+++ htdocs/ssw2.php
@@ -1,34 +1,29 @@
+<?php
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Infinite Mosaic - ssw2</title>
-<style type="text/css" media="screen">
-		<!--
-html,body{
-	height: 100%;
-	margin: 0;
+if(strstr($_SERVER['HTTP_USER_AGENT'], 'iPhone')){
+	header('Location: deepzoom://dz.gmapuploader.com/7kBfnUeTsS.xml');
+	exit();
 }
-div#map{
-	height: 100%;
-	margin: 0;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Infinite Mosaic for Seattle Startup Weekend 2 (ssw2)</title>
+<style>
+img {
+	border: none;
 }
--->
 </style>
-		<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAIgkf_4LoiNSc_U8b1dpNDRQgOj4wU9QB2RGC9-bdJnESUNrTNBR1AC9vfcP3RoYOj2QYq2W18mIGrQ" type="text/javascript"></script>
-		<script src="http://gmapuploader.com/js/gmapuploader.8.js" type="text/javascript"></script>
-	</head>
-	<body onunload="GUnload()">
-		<div id="map"></div>
-		<script type="text/javascript">
-//<![CDATA[
-var map = new GMap2(document.getElementById("map"));
-document.getElementById('map').style.backgroundColor = 'white';
-var mapType = new GmapUploaderMapType(map, "http://mt.gmapuploader.com/tiles/6aoN06dmbF", "jpg", 6);
-map.setCenter(new GLatLng(0,0), 1, mapType);
-map.addControl(new GLargeMapControl());
-map.enableContinuousZoom();
-//]]>
-</script>
-	</body>
+</head>
+<body>
+<center>
+<div>
+<a href="http://blog.infinitemosaic.com"><img src="/images/logo.png" width="295" height="83"></a><br>
+<a href="http://blog.infinitemosaic.com">More Info</a> | <a href="http://blog.infinitemosaic.com/?page_id=20">Sign Up</a>
+</div>
+</center>
+<script type="text/javascript" src="http://seadragon.com/ajax/embed.js"></script><script type="text/javascript">Seadragon.embed("100%", "512px", "http://dz.gmapuploader.com/7kBfnUeTsS.xml", 8192, 8192, 256, 0, "jpg");</script>
+</body>
 </html>
+
